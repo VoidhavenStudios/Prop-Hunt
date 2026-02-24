5855e43fee21d128778c4d26760554f8abb78e36
Voidhaven Studios <rhalza.dev+voidhavenstudios@gmail.com>
2026-02-24 18:23:41 -0500

    Create entities.js

---
+++ js/entities.js
@@ -0,0 +1,98 @@
+class Block extends PhysicsBody {
+    constructor(x, y, imgId) {
+        const img = document.getElementById(imgId);
+        super(x, y, img.width, img.height, true);
+        this.image = img;
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.image, this.x, this.y);
+    }
+}
+
+class Prop extends PhysicsBody {
+    constructor(x, y, typeIndex) {
+        const imgId = `tex-crate${typeIndex}`;
+        const img = document.getElementById(imgId);
+        super(x, y, img.width, img.height, false);
+        this.image = img;
+        this.typeIndex = typeIndex;
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.image, this.x, this.y);
+    }
+}
+
+class Player extends PhysicsBody {
+    constructor(x, y) {
+        const img = document.getElementById('tex-player');
+        super(x, y, img.width, img.height, false);
+        this.originalImage = img;
+        this.currentImage = img;
+        this.isDisguised = false;
+    }
+
+    handleInput(input, props) {
+        if (input.keys['KeyA'] || input.keys['ArrowLeft']) {
+            this.vx -= 1;
+        }
+        if (input.keys['KeyD'] || input.keys['ArrowRight']) {
+            this.vx += 1;
+        }
+        if ((input.keys['KeyW'] || input.keys['ArrowUp'] || input.keys['Space']) && this.grounded) {
+            this.vy = -CONFIG.jumpForce;
+            this.grounded = false;
+        }
+
+        if (input.mouse.rightDown) {
+            this.resetDisguise();
+        }
+
+        if (input.mouse.leftDown) {
+            this.tryMorph(input.mouse, props);
+            input.mouse.leftDown = false; 
+        }
+    }
+
+    tryMorph(mouse, props) {
+        for (let prop of props) {
+            if (checkAABB({x: mouse.x, y: mouse.y, w: 1, h: 1}, prop)) {
+                const dist = getDistance(this.x + this.w/2, this.y + this.h/2, prop.x + prop.w/2, prop.y + prop.h/2);
+                if (dist <= CONFIG.reachDistance) {
+                    this.becomeProp(prop);
+                    return;
+                }
+            }
+        }
+    }
+
+    becomeProp(prop) {
+        this.isDisguised = true;
+        this.currentImage = prop.image;
+        this.w = prop.w;
+        this.h = prop.h;
+        
+        this.y -= 5;
+    }
+
+    resetDisguise() {
+        this.isDisguised = false;
+        this.currentImage = this.originalImage;
+        this.w = this.originalImage.width;
+        this.h = this.originalImage.height;
+        this.y -= 5;
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.currentImage, this.x, this.y, this.w, this.h);
+        
+        if (!this.isDisguised) {
+            ctx.strokeStyle = "rgba(255, 255, 255, 0.5)";
+            ctx.lineWidth = 2;
+            ctx.beginPath();
+            ctx.arc(this.x + this.w/2, this.y + this.h/2, CONFIG.reachDistance, 0, Math.PI * 2);
+            ctx.stroke();
+        }
+    }
+}
