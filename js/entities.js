6583080279b106338e70a62b89888d97aaaf186f
Voidhaven Studios <rhalza.dev+voidhavenstudios@gmail.com>
2026-02-24 18:58:40 -0500

    Update entities.js

--- js/entities.js
+++ js/entities.js
@@ -26,23 +26,36 @@ class Prop extends PhysicsBody {
     constructor(x, y, typeIndex) {
         const imgId = `tex-crate${typeIndex}`;
         const img = document.getElementById(imgId);
-        super(x, y, img.width, img.height, false);
+        const tightBox = getTightHitbox(img);
+        
+        super(x + tightBox.x, y + tightBox.y, tightBox.w, tightBox.h, false);
+        
         this.image = img;
         this.typeIndex = typeIndex;
+        this.hitboxOffset = { x: tightBox.x, y: tightBox.y };
+        this.fullWidth = img.width;
+        this.fullHeight = img.height;
     }
 
     draw(ctx) {
-        ctx.drawImage(this.image, this.x, this.y);
+        ctx.drawImage(this.image, this.x - this.hitboxOffset.x, this.y - this.hitboxOffset.y);
     }
 }
 
 class Player extends PhysicsBody {
     constructor(x, y) {
         const img = document.getElementById('tex-player');
-        super(x, y, img.width, img.height, false);
+        const tightBox = getTightHitbox(img);
+        super(x + tightBox.x, y + tightBox.y, tightBox.w, tightBox.h, false);
+        
         this.originalImage = img;
         this.currentImage = img;
         this.isDisguised = false;
+        
+        this.hitboxOffset = { x: tightBox.x, y: tightBox.y };
+        this.originalStats = { w: tightBox.w, h: tightBox.h, offX: tightBox.x, offY: tightBox.y };
+        
+        this.reticle = { x: 0, y: 0 };
     }
 
     handleInput(input, worldMouse, props) {
@@ -52,29 +65,51 @@ class Player extends PhysicsBody {
         if (input.keys['KeyD'] || input.keys['ArrowRight']) {
             this.vx += 1;
         }
+        
+        let jForce = CONFIG.baseJumpForce;
+        if (this.isDisguised) {
+            const sizeFactor = (this.w * this.h) / 2000; 
+            jForce += Math.min(sizeFactor, 5); 
+        }
+
         if ((input.keys['KeyW'] || input.keys['ArrowUp'] || input.keys['Space']) && this.grounded) {
-            this.vy = -CONFIG.jumpForce;
+            this.vy = -jForce;
             this.grounded = false;
         }
+
+        const dx = worldMouse.x - (this.x + this.w/2);
+        const dy = worldMouse.y - (this.y + this.h/2);
+        const dist = Math.sqrt(dx*dx + dy*dy);
+        const angle = Math.atan2(dy, dx);
+        
+        const clampDist = Math.min(dist, CONFIG.reachDistance);
+        
+        this.reticle.x = (this.x + this.w/2) + Math.cos(angle) * clampDist;
+        this.reticle.y = (this.y + this.h/2) + Math.sin(angle) * clampDist;
 
         if (input.mouse.rightDown) {
             this.resetDisguise();
         }
 
         if (input.mouse.leftDown) {
-            this.tryMorph(worldMouse, props);
+            this.tryMorph(props);
             input.mouse.leftDown = false; 
         }
     }
 
-    tryMorph(mouse, props) {
+    tryMorph(props) {
+        const rSize = 4;
+        const pointerRect = { 
+            x: this.reticle.x - rSize/2, 
+            y: this.reticle.y - rSize/2, 
+            w: rSize, 
+            h: rSize 
+        };
+
         for (let prop of props) {
-            if (checkAABB({x: mouse.x, y: mouse.y, w: 1, h: 1}, prop)) {
-                const dist = getDistance(this.x + this.w/2, this.y + this.h/2, prop.x + prop.w/2, prop.y + prop.h/2);
-                if (dist <= CONFIG.reachDistance) {
-                    this.becomeProp(prop);
-                    return;
-                }
+            if (checkAABB(pointerRect, prop)) {
+                 this.becomeProp(prop);
+                 return;
             }
         }
     }
@@ -84,26 +119,25 @@ class Player extends PhysicsBody {
         this.currentImage = prop.image;
         this.w = prop.w;
         this.h = prop.h;
-        this.y -= 5;
+        this.hitboxOffset = { x: prop.hitboxOffset.x, y: prop.hitboxOffset.y };
+        this.y -= 5; 
     }
 
     resetDisguise() {
         this.isDisguised = false;
         this.currentImage = this.originalImage;
-        this.w = this.originalImage.width;
-        this.h = this.originalImage.height;
+        this.w = this.originalStats.w;
+        this.h = this.originalStats.h;
+        this.hitboxOffset = { x: this.originalStats.offX, y: this.originalStats.offY };
         this.y -= 5;
     }
 
     draw(ctx) {
-        ctx.drawImage(this.currentImage, this.x, this.y, this.w, this.h);
+        ctx.drawImage(this.currentImage, this.x - this.hitboxOffset.x, this.y - this.hitboxOffset.y);
         
-        if (!this.isDisguised) {
-            ctx.strokeStyle = "rgba(255, 255, 255, 0.3)";
-            ctx.lineWidth = 2;
-            ctx.beginPath();
-            ctx.arc(this.x + this.w/2, this.y + this.h/2, CONFIG.reachDistance, 0, Math.PI * 2);
-            ctx.stroke();
-        }
+        ctx.fillStyle = "#FF0000";
+        ctx.beginPath();
+        ctx.arc(this.reticle.x, this.reticle.y, 3, 0, Math.PI * 2);
+        ctx.fill();
     }
 }
