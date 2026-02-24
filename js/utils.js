92e69479134f7e1d0f49b0d6c1e2b0dcdaa0e740
Voidhaven Studios <rhalza.dev+voidhavenstudios@gmail.com>
2026-02-24 18:22:36 -0500

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    gravity: 0.5,
+    friction: 0.8,
+    speed: 5,
+    jumpForce: 12,
+    reachDistance: 150
+};
+
+function checkAABB(rect1, rect2) {
+    return (
+        rect1.x < rect2.x + rect2.w &&
+        rect1.x + rect1.w > rect2.x &&
+        rect1.y < rect2.y + rect2.h &&
+        rect1.y + rect1.h > rect2.y
+    );
+}
+
+function getDistance(x1, y1, x2, y2) {
+    let a = x1 - x2;
+    let b = y1 - y2;
+    return Math.sqrt(a * a + b * b);
+}
