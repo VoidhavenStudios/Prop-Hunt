fd7c85f552c86040d4bf7ba91c923b5e7c6eade2
Voidhaven Studios <rhalza.dev+voidhavenstudios@gmail.com>
2026-02-24 19:33:21 -0500

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,11 +1,15 @@
 class InputHandler {
     constructor(canvas) {
         this.keys = {};
-        this.mouse = { x: 0, y: 0, leftDown: false, rightDown: false };
+        this.mouse = { x: 0, y: 0, leftDown: false, rightDown: false, middleDown: false };
         this.canvas = canvas;
+        this.onPauseToggle = null;
 
         window.addEventListener('keydown', (e) => {
             this.keys[e.code] = true;
+            if (e.code === 'Escape' && this.onPauseToggle) {
+                this.onPauseToggle();
+            }
         });
 
         window.addEventListener('keyup', (e) => {
@@ -20,14 +24,20 @@ class InputHandler {
 
         window.addEventListener('mousedown', (e) => {
             if (e.button === 0) this.mouse.leftDown = true;
+            if (e.button === 1) { 
+                this.mouse.middleDown = true; 
+                e.preventDefault(); 
+            }
             if (e.button === 2) this.mouse.rightDown = true;
         });
 
         window.addEventListener('mouseup', (e) => {
             if (e.button === 0) this.mouse.leftDown = false;
+            if (e.button === 1) this.mouse.middleDown = false;
             if (e.button === 2) this.mouse.rightDown = false;
         });
 
         canvas.addEventListener('contextmenu', e => e.preventDefault());
+        window.addEventListener('wheel', e => { if(e.ctrlKey) e.preventDefault(); }, {passive: false});
     }
 }
