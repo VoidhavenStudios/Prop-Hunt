79364d689b1dce6a9bc3e66d718bcd58c57ce5c1
Voidhaven Studios <rhalza.dev+voidhavenstudios@gmail.com>
2026-02-24 21:28:42 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    gravity: 1.2,
+    groundFriction: 0.85,
+    airFriction: 0.96,
+    groundAccel: 2.5,
+    airAccel: 0.8,
+    maxSpeed: 16.0,
+    baseJumpForce: 24.0,
+    reachDistance: 800,
+    worldScale: 0.5,
+    mapWidth: 4000,
+    mapHeight: 3000,
+    angularDrag: 0.92,
+    maxAngularVelocity: 0.15
+};
