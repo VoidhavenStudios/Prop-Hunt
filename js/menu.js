7734cccc82a62616e6170e75ae78beedc5a6756b
Voidhaven Studios <rhalza.dev+voidhavenstudios@gmail.com>
2026-02-25 00:04:13 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,38 +1,32 @@
 export class MenuController {
     constructor(inputHandler, toggleTeamCallback, spawnPlayerCallback) {
-        console.info("Initializing MenuController");
         this.menuEl = document.getElementById('pause-menu');
         this.btnResume = document.getElementById('btn-resume');
         this.btnTeam = document.getElementById('btn-team');
         this.btnExit = document.getElementById('btn-exit');
-        if (!this.menuEl || !this.btnResume || !this.btnTeam || !this.btnExit) {
-            console.error("MenuController missing required DOM elements");
-        }
         this.input = inputHandler;
         this.isPaused = false;
-        this.input.onPauseToggle = () => this.togglePause();
-        this.btnResume.onclick = () => {
-            console.debug("Resume button clicked");
-            this.togglePause();
-        };
-        this.btnExit.onclick = () => {
-            console.info("Exit/Restart button clicked, reloading page");
-            window.location.reload();
-        };
-        this.btnTeam.onclick = () => {
-            console.info("Change Team button clicked");
-            toggleTeamCallback();
-            spawnPlayerCallback();
-            this.togglePause();
-        };
+        if (this.input) {
+            this.input.onPauseToggle = () => this.togglePause();
+        }
+        if (this.btnResume) this.btnResume.onclick = () => this.togglePause();
+        if (this.btnExit) this.btnExit.onclick = () => window.location.reload();
+        if (this.btnTeam) {
+            this.btnTeam.onclick = () => {
+                toggleTeamCallback();
+                spawnPlayerCallback();
+                this.togglePause();
+            };
+        }
     }
     togglePause() {
         this.isPaused = !this.isPaused;
-        console.info(`Game paused state changed to: ${this.isPaused}`);
-        if (this.isPaused) {
-            this.menuEl.classList.remove('hidden');
-        } else {
-            this.menuEl.classList.add('hidden');
+        if (this.menuEl) {
+            if (this.isPaused) {
+                this.menuEl.classList.remove('hidden');
+            } else {
+                this.menuEl.classList.add('hidden');
+            }
         }
     }
 }
