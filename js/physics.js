ed0b5b6430e99a16e8013a75d94f38f253018885
Voidhaven Studios <rhalza.dev+voidhavenstudios@gmail.com>
2026-02-24 20:19:25 -0500

    Update physics.js

--- js/physics.js
+++ js/physics.js
@@ -9,18 +9,20 @@ class PhysicsBody {
         this.isStatic = isStatic;
         this.grounded = false;
         this.box = { x: 0, y: 0, w: w, h: h };
-        
         this.angle = 0;
         this.angularVelocity = 0;
+        this.isHeld = false;
     }
 
     update() {
-        if (!this.isStatic) {
+        if (!this.isStatic && !this.isHeld) {
             this.vy += CONFIG.gravity;
-            
             const friction = this.grounded ? CONFIG.groundFriction : CONFIG.airFriction;
             this.vx *= friction;
+            
             this.angularVelocity *= CONFIG.angularDrag;
+            if (this.angularVelocity > CONFIG.maxAngularVelocity) this.angularVelocity = CONFIG.maxAngularVelocity;
+            if (this.angularVelocity < -CONFIG.maxAngularVelocity) this.angularVelocity = -CONFIG.maxAngularVelocity;
 
             this.x += this.vx;
             this.y += this.vy;
@@ -32,35 +34,98 @@ class PhysicsBody {
         this.box = calculateTightHitbox(img);
     }
 
+    getVertices() {
+        const cx = this.x + this.box.x + this.box.w / 2;
+        const cy = this.y + this.box.y + this.box.h / 2;
+        const hw = this.box.w / 2;
+        const hh = this.box.h / 2;
+        const cos = Math.cos(this.angle);
+        const sin = Math.sin(this.angle);
+
+        const rotate = (dx, dy) => ({
+            x: cx + dx * cos - dy * sin,
+            y: cy + dx * sin + dy * cos
+        });
+
+        return [
+            rotate(-hw, -hh),
+            rotate(hw, -hh),
+            rotate(hw, hh),
+            rotate(-hw, hh)
+        ];
+    }
+
     resolveCollision(other) {
-        const r1 = getHitbox(this);
-        const r2 = getHitbox(other);
+        if (this.isHeld || other.isHeld) return false;
 
-        if (checkAABB(r1, r2)) {
-            let overlapX = (r1.w / 2 + r2.w / 2) - Math.abs((r1.x + r1.w / 2) - (r2.x + r2.w / 2));
-            let overlapY = (r1.h / 2 + r2.h / 2) - Math.abs((r1.y + r1.h / 2) - (r2.y + r2.h / 2));
+        const v1 = this.getVertices();
+        const v2 = other.getVertices();
+        const axes = getAxes(v1).concat(getAxes(v2));
 
-            if (overlapX < overlapY) {
-                if (r1.x < r2.x) this.x -= overlapX;
-                else this.x += overlapX;
-                this.vx *= 0.5;
-                if(!this.isStatic && !other.isStatic) {
-                    this.angularVelocity += (this.vy * 0.01);
-                }
-            } else {
-                if (r1.y < r2.y) {
-                    this.y -= overlapY;
-                    this.grounded = true;
-                } else {
-                    this.y += overlapY;
-                }
-                this.vy = 0;
-                if(!this.isStatic && Math.abs(this.vx) > 1) {
-                     this.angularVelocity += (this.vx * 0.02);
-                }
+        let minOverlap = Infinity;
+        let mtv = null;
+
+        for (const axis of axes) {
+            const p1 = project(v1, axis);
+            const p2 = project(v2, axis);
+
+            if (p1.max < p2.min || p2.max < p1.min) {
+                return false;
             }
-            return true;
+
+            const overlap = Math.min(p1.max - p2.min, p2.max - p1.min);
+            if (overlap < minOverlap) {
+                minOverlap = overlap;
+                mtv = axis;
+            }
         }
-        return false;
+
+        const cx1 = this.x + this.box.x + this.box.w / 2;
+        const cy1 = this.y + this.box.y + this.box.h / 2;
+        const cx2 = other.x + other.box.x + other.box.w / 2;
+        const cy2 = other.y + other.box.y + other.box.h / 2;
+
+        const dx = cx1 - cx2;
+        const dy = cy1 - cy2;
+        if (dx * mtv.x + dy * mtv.y < 0) {
+            mtv.x = -mtv.x;
+            mtv.y = -mtv.y;
+        }
+
+        if (!this.isStatic && other.isStatic) {
+            this.x += mtv.x * minOverlap;
+            this.y += mtv.y * minOverlap;
+            this.applyCollisionResponse(mtv, other);
+        } else if (this.isStatic && !other.isStatic) {
+            other.x -= mtv.x * minOverlap;
+            other.y -= mtv.y * minOverlap;
+            other.applyCollisionResponse({ x: -mtv.x, y: -mtv.y }, this);
+        } else if (!this.isStatic && !other.isStatic) {
+            const shift = minOverlap / 2;
+            this.x += mtv.x * shift;
+            this.y += mtv.y * shift;
+            other.x -= mtv.x * shift;
+            other.y -= mtv.y * shift;
+            this.applyCollisionResponse(mtv, other);
+            other.applyCollisionResponse({ x: -mtv.x, y: -mtv.y }, this);
+        }
+
+        return true;
+    }
+
+    applyCollisionResponse(normal, other) {
+        if (normal.y < -0.5) this.grounded = true;
+
+        const dot = this.vx * normal.x + this.vy * normal.y;
+        if (dot < 0) {
+            this.vx -= dot * normal.x * 1.5;
+            this.vy -= dot * normal.y * 1.5;
+        }
+
+        if (Math.abs(normal.x) > Math.abs(normal.y)) {
+            this.angularVelocity -= this.vy * 0.005 * Math.sign(normal.x);
+        } else {
+            this.angularVelocity += this.vx * 0.005 * Math.sign(normal.y);
+        }
     }
 }
