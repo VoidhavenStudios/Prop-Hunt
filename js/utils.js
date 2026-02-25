87d531f395b8f9fbefa252edfa16c09596fca210
Voidhaven Studios <rhalza.dev+voidhavenstudios@gmail.com>
2026-02-24 20:18:40 -0500

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,31 +1,17 @@
 const CONFIG = {
-    gravity: 0.5,
-    friction: 0.95, 
+    gravity: 1.0,
+    friction: 0.95,
     groundFriction: 0.9,
     airFriction: 0.98,
     speed: 5,
-    baseJumpForce: 14,
-    reachDistance: 700, 
+    baseJumpForce: 19.8,
+    reachDistance: 700,
     worldScale: 0.5,
     mapWidth: 4000,
     mapHeight: 3000,
-    angularDrag: 0.95
+    angularDrag: 0.95,
+    maxAngularVelocity: 0.25
 };
-
-function checkAABB(rect1, rect2) {
-    return (
-        rect1.x < rect2.x + rect2.w &&
-        rect1.x + rect1.w > rect2.x &&
-        rect1.y < rect2.y + rect2.h &&
-        rect1.y + rect1.h > rect2.y
-    );
-}
-
-function getDistance(x1, y1, x2, y2) {
-    let a = x1 - x2;
-    let b = y1 - y2;
-    return Math.sqrt(a * a + b * b);
-}
 
 function getHitbox(entity) {
     return {
@@ -61,4 +47,39 @@ function calculateTightHitbox(img) {
     } catch (e) {
         return { x: 0, y: 0, w: img.width, h: img.height };
     }
+}
+
+function getAxes(vertices) {
+    const axes = [];
+    for (let i = 0; i < vertices.length; i++) {
+        const p1 = vertices[i];
+        const p2 = vertices[(i + 1) % vertices.length];
+        const edge = { x: p2.x - p1.x, y: p2.y - p1.y };
+        const normal = { x: -edge.y, y: edge.x };
+        const len = Math.sqrt(normal.x * normal.x + normal.y * normal.y);
+        axes.push({ x: normal.x / len, y: normal.y / len });
+    }
+    return axes;
+}
+
+function project(vertices, axis) {
+    let min = Infinity, max = -Infinity;
+    for (const v of vertices) {
+        const proj = v.x * axis.x + v.y * axis.y;
+        if (proj < min) min = proj;
+        if (proj > max) max = proj;
+    }
+    return { min, max };
+}
+
+function pointInPolygon(point, vertices) {
+    let inside = false;
+    for (let i = 0, j = vertices.length - 1; i < vertices.length; j = i++) {
+        const xi = vertices[i].x, yi = vertices[i].y;
+        const xj = vertices[j].x, yj = vertices[j].y;
+        const intersect = ((yi > point.y) !== (yj > point.y))
+            && (point.x < (xj - xi) * (point.y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    return inside;
 }
