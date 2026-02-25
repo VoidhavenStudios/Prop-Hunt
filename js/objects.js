e9ca34dd1935b064f3256cffe933d5916fff2115
Voidhaven Studios <rhalza.dev+voidhavenstudios@gmail.com>
2026-02-24 22:22:15 -0500

    Create objects.js

---
+++ js/objects.js
@@ -0,0 +1,8 @@
+export const PROP_TYPES = {
+    0: { imgId: 'tex-crate0', mass: 15 },
+    1: { imgId: 'tex-crate1', mass: 15 },
+    2: { imgId: 'tex-crate2', mass: 15 },
+    3: { imgId: 'tex-crate3', mass: 15 },
+    4: { imgId: 'tex-crate4', mass: 15 },
+    5: { imgId: 'tex-crate5', mass: 15 }
+};
