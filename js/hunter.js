0d22fcce8a701756663abe92f47ab906f955cb36
Voidhaven Studios <rhalza.dev+voidhavenstudios@gmail.com>
2026-02-24 20:20:10 -0500

    Update hunter.js

--- js/hunter.js
+++ js/hunter.js
@@ -8,7 +8,6 @@ class HunterPlayer extends BasePlayer {
             document.getElementById('tex-gun3')
         ];
         this.currentGunIndex = 0;
-        this.gunOffset = 0; 
     }
 
     handleSpecificInput(input, entities) {
@@ -17,47 +16,34 @@ class HunterPlayer extends BasePlayer {
         if (input.keys['Digit3']) this.currentGunIndex = 2;
         if (input.keys['Digit4']) this.currentGunIndex = 3;
 
-        if (input.mouse.leftDown) {
-            // Shoot logic (placeholder)
-        }
-        
-        if (input.mouse.rightDown) {
-            // Pickup logic (placeholder)
+        if (input.mouse.leftPressed) {
         }
         
         if (input.mouse.middleDown) {
-            // Special logic (placeholder)
         }
     }
 
     draw(ctx) {
-        // Draw Player
         ctx.drawImage(this.originalImage, this.x, this.y);
 
-        // Draw Gun
         const gunImg = this.guns[this.currentGunIndex];
-        const centerX = this.x + this.w / 2;
-        const centerY = this.y + this.h / 2;
         
-        // Calculate angle to cursor
-        const dx = this.cursor.x - centerX;
-        const dy = this.cursor.y - centerY;
+        const anchorX = this.facingRight ? this.x + this.w : this.x;
+        const anchorY = this.y + this.h * 0.4;
+        
+        const dx = this.cursor.x - anchorX;
+        const dy = this.cursor.y - anchorY;
         const angle = Math.atan2(dy, dx);
 
-        // Gun Anchor Position (Edge of sprite)
-        const anchorDist = this.w / 2;
-        const gunX = centerX + Math.cos(angle) * anchorDist;
-        const gunY = centerY + Math.sin(angle) * anchorDist;
-
         ctx.save();
-        ctx.translate(gunX, gunY);
+        ctx.translate(anchorX, anchorY);
         ctx.rotate(angle);
         
         if (Math.abs(angle) > Math.PI / 2) {
             ctx.scale(1, -1);
         }
 
-        ctx.drawImage(gunImg, 0, -gunImg.height / 2);
+        ctx.drawImage(gunImg, -gunImg.width / 2, -gunImg.height / 2);
         ctx.restore();
 
         this.drawCursor(ctx);
