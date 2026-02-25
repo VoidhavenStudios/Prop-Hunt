48aeade63a1ac3f2f793019f78e9c7633cca96d7
Voidhaven Studios <rhalza.dev+voidhavenstudios@gmail.com>
2026-02-24 19:02:28 -0500

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,12 @@
 const CONFIG = {
-    gravity: 0.6,
-    friction: 0.85,
+    gravity: 0.5,
+    friction: 0.8,
     speed: 5,
     baseJumpForce: 14,
-    reachDistance: 450, 
-    worldScale: 0.6
+    reachDistance: 450,
+    worldScale: 0.5,
+    mapWidth: 3000,
+    mapHeight: 2000
 };
 
 function checkAABB(rect1, rect2) {
@@ -22,36 +24,11 @@ function getDistance(x1, y1, x2, y2) {
     return Math.sqrt(a * a + b * b);
 }
 
-function getTightHitbox(img) {
-    const canvas = document.createElement('canvas');
-    canvas.width = img.width;
-    canvas.height = img.height;
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0);
-    const data = ctx.getImageData(0, 0, img.width, img.height).data;
-
-    let left = img.width, right = 0, top = img.height, bottom = 0;
-    let found = false;
-
-    for (let y = 0; y < img.height; y++) {
-        for (let x = 0; x < img.width; x++) {
-            const alpha = data[(y * img.width + x) * 4 + 3];
-            if (alpha > 20) {
-                if (x < left) left = x;
-                if (x > right) right = x;
-                if (y < top) top = y;
-                if (y > bottom) bottom = y;
-                found = true;
-            }
-        }
-    }
-
-    if (!found) return { x: 0, y: 0, w: img.width, h: img.height };
-
+function getHitbox(entity) {
     return {
-        x: left,
-        y: top,
-        w: right - left + 1,
-        h: bottom - top + 1
+        x: entity.x + entity.box.x,
+        y: entity.y + entity.box.y,
+        w: entity.box.w,
+        h: entity.box.h
     };
 }
